c42bef3d68dcda264264a310f1b25298b8c04ccd
mmuddy <yakovlevvladyakovlev@yandex.ru>
2016-10-27 12:00:13 +0300

    rewrite urls for new view

--- taskgraph/urls.py
+++ taskgraph/urls.py
@@ -1,23 +1,25 @@
 """taskgraph URL Configuration"""
 
-from . import views
+from taskgraph.view import base, profile, graph
 
 from django.conf.urls import url
 
 
 urlpatterns = [
 
-    url(r'^$', views.project_overview_page, name='overview'),
+    url(r'^$', base.overview_page, name='overview'),
 
-    url(r'signup/', views.signup_page, name='signup'),
+    url(r'signup/', base.signup_page, name='signup'),
 
-    url(r'edit/', views.edit_page, name='edit'),
-    url(r'projects/', views.projects_page, name='projects'),
-    url(r'view/', views.graph_view_page, name='view'),
-    url(r'analysis/', views.analysis_page, name='analysis'),
+    url(r'graph-edit/', graph.edit_page, name='edit'),
+    url(r'projects/', graph.projects_page, name='projects'),
+    url(r'view/', graph.graph_view_page, name='view'),
+    url(r'analysis/', graph.analysis_page, name='analysis'),
 
-    url(r'trackers/', views.trackers_page, name='trackers'),
-    url(r'trackers-list/', views.trackers_list_page, name='trackers-list'),
-    url(r'trackers-add/', views.trackers_add_page, name='trackers-add'),
+    url(r'trackers/', profile.trackers_page, name='trackers'),
+    url(r'trackers-list/', profile.trackers_list_page, name='trackers-list'),
+    url(r'trackers-add/', profile.trackers_add_page, name='trackers-add'),
+    url(r'trackers-edit/(-?\d+)/', profile.trackers_edit_page, name='trackers-edit'),
+    url(r'trackers-delete/(-?\d+)/', profile.trackers_delete, name='trackers-delete'),
 
 ]
