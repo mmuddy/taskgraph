66c1889faa853c8bbbc8eef9e1b15ef949cf4b3a
mmuddy <yakovlevvladyakovlev@yandex.ru>
2016-10-28 09:17:32 +0300

    fix

--- taskgraphproject/settings.py
+++ taskgraphproject/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taskgraph',
+    'taskgraph'
 ]
 
 MIDDLEWARE_CLASSES = [
