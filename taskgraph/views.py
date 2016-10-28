b5004e04e0685e22bb88d1e0dda2004ef8f0c6af
aaaaaa2493 <aaaaaa2493@yandex.ru>
2016-10-28 22:25:32 +0300

    Add render graph to view

--- taskgraph/views.py
+++ taskgraph/views.py
@@ -20,8 +20,47 @@ def projects_page(request):
 
 
 def graph_view_page(request):
+
+    info = [
+        [1, 'Task1'],
+        [2,'Task2'],
+        [3,'Task3'],
+        [4,'Task4'],
+        [5,'Task5'],
+        [6,'Task6'],
+        [7,'Task7'],
+        [8,'Task8'],
+        [9,'Task9'],
+        [10,'Task10'],
+        [11,'Task11'],
+        [12,'Task12'],
+        [13,'Task13'],
+    ]
+
+
+    graph = {
+        1: [2,3,4,13],
+        2: [5,6],
+        3: [5,6,13],
+        4: [6],
+        5: [7,13],
+        6: [7, 8],
+        7: [9, 10],
+        8: [10, 11],
+        9: [12, 13],
+        10: [12],
+        11: [12, 13],
+        12: [13],
+        13: [],
+    }
+
+
+    all_edges = [(i,j) for i in graph for j in graph[i]]
+
     context = {'is_user_active': True,
-               'contains_menu': True}
+               'contains_menu': True,
+               'all_edges': all_edges,
+               'info': info}
     return render(request, 'taskgraph/graph/view.html', context)
 
 
