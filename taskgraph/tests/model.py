7fb828afca44eeb51cb150876de9fa9f0b5c97ac
mmuddy <yakovlevvladyakovlev@yandex.ru>
2016-10-28 21:33:49 +0300

    fix @staticmethod's

--- taskgraph/tests/model.py
+++ taskgraph/tests/model.py
@@ -36,80 +36,77 @@ class TestTracker(TestCase):
 
         self.assertTrue(False)
 
-    @staticmethod
-    def assert_creation(test_case, models_before):
-        for model_type, objects_before in models_before:
-            test_case.assertTrue(model_type.objects.all().count() - objects_before > 0)
 
-    @staticmethod
-    def assert_cleanup(test_case, models_before):
-        for model_type, objects_before in models_before:
-            test_case.assertTrue(model_type.objects.all().count() - objects_before == 0)
-
-    @staticmethod
-    def test_projects_creation_and_cleanup(test_case, tracker):
-        type_list = [Project, Assignee, TaskState, TaskRelationType]
-
-        models_before = []
-        for model_type in type_list:
-            models_before.append((model_type, model_type.objects.all().count()))
-
-        tracker.restore_project_list(get_interface(tracker.type))
-
-        TestTracker.assert_creation(test_case, models_before)
-
-        tracker.delete()
-
-        TestTracker.assert_cleanup(test_case, models_before)
+def assert_creation(test_case, models_before):
+    for model_type, objects_before in models_before:
+        test_case.assertTrue(model_type.objects.all().count() - objects_before > 0)
 
 
-class TestProject:
+def assert_cleanup(test_case, models_before):
+    for model_type, objects_before in models_before:
+        test_case.assertTrue(model_type.objects.all().count() - objects_before == 0)
 
-    @staticmethod
-    def test_create_and_clean_up_tasks(test_case, tracker):
-        tracker.restore_project_list(get_interface(tracker.type))
 
-        list_before = []
+def test_projects_creation_and_cleanup(test_case, tracker):
+    type_list = [Project, Assignee, TaskState, TaskRelationType]
 
-        task_count = Task.objects.all().count()
-        rel_count = TaskRelation.objects.all().count()
+    models_before = []
+    for model_type in type_list:
+        models_before.append((model_type, model_type.objects.all().count()))
 
-        list_before.append((Task, task_count))
-        #list_before.append((TaskRelation, rel_count))
+    tracker.restore_project_list(get_interface(tracker.type))
 
-        for project in tracker.project_set.all():
-            project.is_active = True
-            project.save()
+    assert_creation(test_case, models_before)
 
-        tracker.restore_project_tasks(get_interface(tracker.type))
+    tracker.delete()
 
-        for model_type, before_count in list_before:
-            test_case.assertTrue(model_type.objects.all().count() - before_count > 0)
+    assert_cleanup(test_case, models_before)
 
-        tracker.delete()
 
-        for model_type, before_count in list_before:
-            test_case.assertTrue(model_type.objects.all().count() - before_count == 0)
+def test_create_and_clean_up_tasks(test_case, tracker):
+    tracker.restore_project_list(get_interface(tracker.type))
+
+    list_before = []
+
+    task_count = Task.objects.all().count()
+    rel_count = TaskRelation.objects.all().count()
+
+    list_before.append((Task, task_count))
+    #list_before.append((TaskRelation, rel_count))
+
+    for project in tracker.project_set.all():
+        project.is_active = True
+        project.save()
+
+    tracker.restore_project_tasks(get_interface(tracker.type))
+
+    for model_type, before_count in list_before:
+        test_case.assertTrue(model_type.objects.all().count() - before_count > 0)
+
+    tracker.delete()
+
+    for model_type, before_count in list_before:
+        test_case.assertTrue(model_type.objects.all().count() - before_count == 0)
 
 
 class TestTrackerWithDummy(TestCase):
 
     def test_projects_creation_and_cleanup(self):
-        TestTracker.test_projects_creation_and_cleanup(self, tracker_dummy())
+        test_projects_creation_and_cleanup(self, tracker_dummy())
 
 
 class TestTrackerWithRedmine(TestCase):
     def test_projects_creation_and_cleanup(self):
         if tracker_redmine:
-            TestTracker.test_projects_creation_and_cleanup(self, tracker_redmine())
+            test_projects_creation_and_cleanup(self, tracker_redmine())
 
 
 class TestProjectWithDummy(TestCase):
     def test_projects_creation_and_cleanup(self):
-        TestProject.test_create_and_clean_up_tasks(self, tracker_dummy())
+        test_create_and_clean_up_tasks(self, tracker_dummy())
 
 
 class TestProjectWithRedmine(TestCase):
     def test_projects_creation_and_cleanup(self):
         if tracker_redmine:
-            TestProject.test_create_and_clean_up_tasks(self, tracker_redmine())
+            test_create_and_clean_up_tasks(self, tracker_redmine())
