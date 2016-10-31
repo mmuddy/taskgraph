aa6b3e8a4a73ae6b2f65ccdea6d55df166e06249
Igor Shadrintcev <mitrich512@gmail.com>
2016-10-31 19:14:51 +0300

    Excess queries removed, refactored

--- taskgraph/view/graph.py
+++ taskgraph/view/graph.py
@@ -31,6 +31,7 @@ def graph_view_page(request):
 
 
 def task_edit_page(request):
+
     try:
         tracker = Tracker.objects.all().get(type='Dummy')
     except Tracker.DoesNotExist:
@@ -38,24 +39,21 @@ def task_edit_page(request):
         tracker.save()
 
     tracker.restore_project_list(get_interface(tracker.type))
-    project = list(Project.objects.all())[0]
+    project = tracker.project_set.all()[0]
     project.restore_project_tasks(get_interface(tracker.type))
-    task = list(Task.objects.all())[0]
+    task = project.task_set.all()[0]
 
     add_fields = []
-    for field in TaskAdditionalField.objects.filter(project = project, task = task):
-        if int(field.type) == 0 :
-            add_fields += [[field.name, '<input value="{}" class="form-control">'.format(field.char)]]
-        if int(field.type) == 1 :
-            add_fields += [[field.name,
-                '<textarea style="margin: 5px; width: 93%" rows="5" class="form-control">{}</textarea>'.format(field.text)]]
-        if int(field.type) == 2 :
-            add_fields += [[field.name, '<input type="date" value="{}" class="form-control"'.format(field.date)]]
+    tags = ('<input value="{}" class="form-control">',
+            '<textarea style="margin: 5px; width: 93%" rows="5" class="form-control">{}</textarea>',
+            '<input type="date" value="{}" class="form-control"')
+    for field in task.taskadditionalfield_set.filter(project = project, task = task):
+        add_fields += [[field.name, tags[int(field.type)].format(field.char)]]
 
-    to_relations = [('Task id: ' + str(r.from_task.identifier) + ', relation: ' + r.type.name)
-                      for r in TaskRelation.objects.filter(project = project, to_task = task)]
-    from_relations = [('Task id: ' + str(r.to_task.identifier) + ', relation: ' + r.type.name)
-                      for r in TaskRelation.objects.filter(project = project, from_task = task)]
+    to_relations = [('Task id: ' + str(i.from_task.identifier) + ', relation: ' + i.type.name)
+                      for i in project.taskrelation_set.filter(project = project, to_task = task)]
+    from_relations = [('Task id: ' + str(i.to_task.identifier) + ', relation: ' + i.type.name)
+                      for i in project.taskrelation_set.filter(project = project, from_task = task)]
 
     context = {'is_user_active': True,
                 'contains_menu': True,
@@ -64,12 +62,12 @@ def task_edit_page(request):
                 'milestone' : task.milestone.name,
                 'category' : task.category.name,
                 'state' : task.state.name,
-                'assignee_list' : [a.name for a in Assignee.objects.filter(project = project)],
-                'milestone_list': [m.name for m in Milestone.objects.filter(project = project)],
-                'category_list': [c.name for c in TaskCategory.objects.filter(project = project)],
-                'state_list': [a.name for a in TaskState.objects.filter(project = project)],
+                'assignee_list' : [i.name for i in project.assignee_set.all()],
+                'milestone_list': [i.name for i in project.milestone_set.all()],
+                'category_list': [i.name for i in project.taskcategory_set.all()],
+                'state_list': [i.name for i in project.taskstate_set.all()],
                 'add_fields': add_fields,
                 'to_relations' : to_relations,
-                'from_relations': from_relations
+                'from_relations': from_relations,
     }
     return render(request, 'taskgraph/graph/task_edit.html', context)
